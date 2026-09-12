0cec1b33deca0ad7b3bd2022ed755a07896bde6b
montalvomoralesmiguelangel2-create <montalvomoralesmiguelangel2@gmail.com>
2026-09-12 17:29:12 -0500

    fix: apuntar checker de updates al fork (miguelam32)

--- service/kotlinYtmusicScraper/src/commonMain/kotlin/com/maxrave/kotlinytmusicscraper/Ytmusic.kt
+++ service/kotlinYtmusicScraper/src/commonMain/kotlin/com/maxrave/kotlinytmusicscraper/Ytmusic.kt
@@ -654,7 +654,7 @@ class Ytmusic {
         }
 
     suspend fun checkForGithubReleaseUpdate() =
-        httpClient.get("https://api.github.com/repos/maxrave-dev/SimpMusic/releases/latest") {
+        httpClient.get("https://api.github.com/repos/miguelam32/SimpMusic/releases/latest") {
             contentType(ContentType.Application.Json)
         }
 
